f8131180519034d9eac3c9cb46ab128f143e5313
M-Gucci <a23.ar6b@g.chuo-u.ac.jp>
2025-10-30 15:18:38 +0900

    1.9倍の駒追加

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,180 +1,36 @@
 # treatPDF_sizing実行用シェルスクリプト
 python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
-1.0
+1.9
 0.2
-bishop_1
-EOF
-
-python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
-1.5
-0.2
-bishop_15
-EOF
-
-python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
-2.0
-0.2
-bishop_2
-EOF
-
-python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
-2.5
-0.2
-bishop_25
-EOF
-
-python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
-3.0
-0.2
-bishop_3
+bishop_19
 EOF
 
 python treatPDF_sizing.py ../models/obj/king.obj << EOF
-1.0
+1.9
 0.2
-king_1
-EOF
-
-python treatPDF_sizing.py ../models/obj/king.obj << EOF
-1.5
-0.2
-king_15
-EOF
-
-python treatPDF_sizing.py ../models/obj/king.obj << EOF
-2.0
-0.2
-king_2
-EOF
-
-python treatPDF_sizing.py ../models/obj/king.obj << EOF
-2.5
-0.2
-king_25
-EOF
-
-python treatPDF_sizing.py ../models/obj/king.obj << EOF
-3.0
-0.2
-king_3
+king_19
 EOF
 
 python treatPDF_sizing.py ../models/obj/knight.obj << EOF
-1.0
+1.9
 0.2
-knight_1
-EOF
-
-python treatPDF_sizing.py ../models/obj/knight.obj << EOF
-1.5
-0.2
-knight_15
-EOF
-
-python treatPDF_sizing.py ../models/obj/knight.obj << EOF
-2.0
-0.2
-knight_2
-EOF
-
-python treatPDF_sizing.py ../models/obj/knight.obj << EOF
-2.5
-0.2
-knight_25
-EOF
-
-python treatPDF_sizing.py ../models/obj/knight.obj << EOF
-3.0
-0.2
-knight_3
+knight_19
 EOF
 
 python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
-1.0
+1.9
 0.2
-pawn_1
-EOF
-
-python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
-1.5
-0.2
-pawn_15
-EOF
-
-python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
-2.0
-0.2
-pawn_2
-EOF
-
-python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
-2.5
-0.2
-pawn_25
-EOF
-
-python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
-3.0
-0.2
-pawn_3
+pawn_19
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween.obj << EOF
-1.0
+1.9
 0.2
-qween_1
-EOF
-
-python treatPDF_sizing.py ../models/obj/qween.obj << EOF
-1.5
-0.2
-qween_15
-EOF
-
-python treatPDF_sizing.py ../models/obj/qween.obj << EOF
-2.0
-0.2
-qween_2
-EOF
-
-python treatPDF_sizing.py ../models/obj/qween.obj << EOF
-2.5
-0.2
-qween_25
-EOF
-
-python treatPDF_sizing.py ../models/obj/qween.obj << EOF
-3.0
-0.2
-qween_3
+qween_19
 EOF
 
 python treatPDF_sizing.py ../models/obj/rook.obj << EOF
-1.0
+1.9
 0.2
-rook_1
-EOF
-
-python treatPDF_sizing.py ../models/obj/rook.obj << EOF
-1.5
-0.2
-rook_15
-EOF
-
-python treatPDF_sizing.py ../models/obj/rook.obj << EOF
-2.0
-0.2
-rook_2
-EOF
-
-python treatPDF_sizing.py ../models/obj/rook.obj << EOF
-2.5
-0.2
-rook_25
-EOF
-
-python treatPDF_sizing.py ../models/obj/rook.obj << EOF
-3.0
-0.2
-rook_3
+rook_19
 EOF
