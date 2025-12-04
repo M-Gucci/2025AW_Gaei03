4804f5bcaa596a56b70c4b6d21ddaa809efa266c
M-Gucci <a23.ar6b@g.chuo-u.ac.jp>
2025-12-04 18:06:37 +0900

    デザインを反映した駒作成

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,20 +1,9 @@
 # treatPDF_sizing実行用シェルスクリプト
 python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
 2.1
-255 255 128
-n
-n
-n
-n
-0.1
-bishop_21_yellow
-EOF
-
-python treatPDF_sizing.py ../models/obj/king.obj << EOF
-2.1
-255 255 128
+126 113 143
 y
-./../photo/King-purple.png
+./../photo/Design/ビショップ-黄色.png
 0.00
 2.765
 1.95
@@ -22,91 +11,130 @@ n
 n
 n
 0.1
-king_21_yellow
+bishop_21_purple
+EOF
+
+python treatPDF_sizing.py ../models/obj/king.obj << EOF
+2.1
+126 113 143
+y
+./../photo/Design/キング-黄色.png
+0.00
+2.765
+1.95
+n
+n
+n
+0.1
+king_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/knight.obj << EOF
 2.1
-255 255 128
-n
+126 113 143
+y
+./../photo/Design/ナイト-黄色.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-knight_21_yellow
+knight_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
 2.1
-255 255 128
-n
+126 113 143
+y
+./../photo/Design/ポーン-黄色.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-pawn_21_yellow
+pawn_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_front.obj << EOF
 2.1
-255 255 128
-n
+126 113 143
+y
+./../photo/Design/クイーン-黄色.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-qween_f_21_yellow
+qween_f_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_back.obj << EOF
 2.1
-255 255 128
+126 113 143
 n
 n
 n
 n
-qween_b_21_yellow
+qween_b_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/rook.obj << EOF
 2.1
-255 255 128
-n
+126 113 143
+y
+./../photo/Design/ルーク-黄色.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-rook_21_yellow
+rook_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/king_tall.obj << EOF
 2.1
-255 255 128
-n
+126 113 143
+y
+./../photo/Design/キング-黄色.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-king_tall_21_yellow
+king_tall_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_front_tall.obj << EOF
 2.1
-255 255 128
-n
+126 113 143
+y
+./../photo/Design/クイーン-黄色.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-qween_f_tall_21_yellow
+qween_f_tall_21_purple
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_back_tall.obj << EOF
 2.1
-255 255 128
+126 113 143
 n
 n
 n
 n
-qween_b_tall_21_yellow
+qween_b_tall_21_purple
 EOF
