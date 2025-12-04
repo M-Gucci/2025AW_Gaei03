04358dffd3682801077147f1a9617a6f2927d453
M-Gucci <a23.ar6b@g.chuo-u.ac.jp>
2025-12-04 11:24:14 +0900

    dekitatoomou

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,108 +1,112 @@
 # treatPDF_sizing実行用シェルスクリプト
 python treatPDF_sizing.py ../models/obj/bishop.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-bishop_21
+bishop_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/king.obj << EOF
 2.1
-100 100 100
-n
+255 255 128
+y
+./../photo/King-purple.png
+0.00
+2.765
+1.95
 n
 n
 n
 0.1
-king_21
+king_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/knight.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-knight_21
+knight_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/pawn.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-pawn_21
+pawn_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_front.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-qween_f_21
+qween_f_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_back.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
-qween_b_21
+qween_b_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/rook.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-rook_21
+rook_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/king_tall.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-king_tall_21
+king_tall_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_front_tall.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
 0.1
-qween_f_tall_21
+qween_f_tall_21_yellow
 EOF
 
 python treatPDF_sizing.py ../models/obj/qween_back_tall.obj << EOF
 2.1
-100 100 100
+255 255 128
 n
 n
 n
 n
-qween_b_tall_21
+qween_b_tall_21_yellow
 EOF
